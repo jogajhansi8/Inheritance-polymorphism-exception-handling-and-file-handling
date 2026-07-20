b2164cb4e036520fd1a5e5211d90c1d6a902f313
jogajhansi8 <jogajhansi8@gmail.com>
2026-07-20 21:53:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+class Area
+{
+    public double CalculateArea(double radius)
+    {
+        return 3.14 * radius * radius;
+    }
+    public double CalculateArea(double length, double width)
+    {
+        return length * width;
+    }
+    public double CalculateArea(double b, double h, int triangle)
+    {
+        return 0.5 * b * h;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Area obj = new Area();
+
+        Console.Write("Enter Radius of Circle: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Area of Circle = " + obj.CalculateArea(radius));
+
+        Console.Write("\nEnter Length of Rectangle: ");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Width of Rectangle: ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Area of Rectangle = " + obj.CalculateArea(length, width));
+
+        Console.Write("\nEnter Base of Triangle: ");
+        double baseValue = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Height of Triangle: ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Area of Triangle = " + obj.CalculateArea(baseValue, height, 1));
+    }
+}
---
+++ compile_time_polymorphism/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class MyMath
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Subtract(int a, int b, int c)
+    {
+        return a - b - c;
+    }
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Multiply(int a, int b, int c)
+    {
+        return a * b * c;
+    }
+    public int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    public int Divide(int a, int b, int c)
+    {
+        return (a / b) / c;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyMath obj = new MyMath();
+
+        Console.WriteLine("Addition");
+        Console.WriteLine("10 + 20 = " + obj.Add(10, 20));
+        Console.WriteLine("10 + 20 + 30 = " + obj.Add(10, 20, 30));
+
+        Console.WriteLine("\nSubtraction");
+        Console.WriteLine("20 - 10 = " + obj.Subtract(20, 10));
+        Console.WriteLine("50 - 20 - 10 = " + obj.Subtract(50, 20, 10));
+
+        Console.WriteLine("\nMultiplication");
+        Console.WriteLine("5 * 4 = " + obj.Multiply(5, 4));
+        Console.WriteLine("2 * 3 * 4 = " + obj.Multiply(2, 3, 4));
+
+        Console.WriteLine("\nDivision");
+        Console.WriteLine("20 / 5 = " + obj.Divide(20, 5));
+        Console.WriteLine("100 / 5 / 2 = " + obj.Divide(100, 5, 2));
+    }
+}
