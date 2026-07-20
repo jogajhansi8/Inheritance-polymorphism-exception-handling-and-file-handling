9ec543a0992295c795f35b1ce06974bbc3db3af5
jogajhansi8 <jogajhansi8@gmail.com>
2026-07-20 21:28:41 +0530

    Add files via upload

---
+++ negative_number_exception/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class NegativeNumberException : Exception
+{
+    public NegativeNumberException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter Marks: ");
+            int marks = Convert.ToInt32(Console.ReadLine());
+
+            if (marks < 0)
+            {
+                throw new NegativeNumberException("Marks cannot be negative.");
+            }
+
+            Console.WriteLine("Marks = " + marks);
+        }
+        catch (NegativeNumberException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Enter only integer values.");
+        }
+    }
+}
